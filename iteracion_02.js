bfcf30d5a82d99dac25abc4541632c30decd86ea
Ruben <ruben.infantes@gmail.com>
2022-03-01 17:13:57 +0100

    iteraction#2 complete

--- iteracion_02.js
+++ iteracion_02.js
@@ -5,3 +5,18 @@ const alumns = [
     { name: 'Alfredo Blanco', T1: false, T2: false, T3: false },
     { name: 'Raquel Benito', T1: true, T2: true, T3: true }
 ]
+
+let isAproved = Boolean;
+let message = "";
+
+for (let i = 0; i < alumns.length; i++) {
+    if ((alumns[i].T1 && alumns[i].T2) || (alumns[i].T3 && alumns[i].T2) || (alumns[i].T1 && alumns[i].T3)) {
+        isAproved = true
+    } else { isAproved = false }
+    if (isAproved === true) {
+        message = "aprobado."
+    } else { message = "suspenso." }
+
+    console.log("El alumno " + alumns[i].name + " esta " + message)
+
+}
